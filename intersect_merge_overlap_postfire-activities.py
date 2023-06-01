20f3d579b1211df20a11158b5ec0c4d03ed38cf4
kellyjsmith <93109030+kellyjsmith@users.noreply.github.com>
2023-06-01 16:19:47 -0700

    Update intersect_merge_overlap_postfire-activities.py

--- intersect_merge_overlap_postfire-activities.py
+++ intersect_merge_overlap_postfire-activities.py
@@ -32,13 +32,13 @@ arcpy.AddField_management(in_table, field_name, "SHORT")
 # Execute CalculateField
 arcpy.CalculateField_management(in_table, field_name, expression, "PYTHON")
 
-# Select records from mtbs_perims_dd that intersect Admin_Forests_R5 and filter where 1993 > Ig_Year < 2017
+# Subset MTBS layer to include only wildfires in R5 from 1993-2017
 arcpy.MakeFeatureLayer_management("mtbs_perims_dd", "mtbs_lyr")
 arcpy.SelectLayerByLocation_management("mtbs_lyr", "INTERSECT", "Admin_Forests_R5")
 arcpy.SelectLayerByAttribute_management("mtbs_lyr", "SUBSET_SELECTION", "Ig_Year > 1993 AND Ig_Year < 2017")
 arcpy.SelectLayerByAttribute_management("mtbs_lyr", "SUBSET_SELECTION", "Incid_Type = 'Wildfire'")
 
-# Calculate geometry using US acres in field "Fire_Acres_R5"
+# Calculate acres in field "Fire_Acres"
 arcpy.AddField_management("mtbs_lyr", "Fire_Acres", "SHORT")
 arcpy.CalculateField_management("mtbs_lyr", "Fire_Acres", "!shape.area@acres!", "PYTHON3")
 
@@ -46,7 +46,7 @@ arcpy.CalculateField_management("mtbs_lyr", "Fire_Acres", "!shape.area@acres!", 
 field_mappings = arcpy.FieldMappings()
 field_mappings.addTable("mtbs_lyr")
 for field in field_mappings.fields:
-    if field.name not in ["Incid_Name", "BurnBndAc", "Ig_Year", "Ig_Date", "Fire_Acres"]:
+    if field.name not in ["Incid_Name", "BurnBndAc", "Ig_Year", "Fire_Acres"]:
         field_mappings.removeFieldMap(field_mappings.findFieldMapIndex(field.name))
 # Save output as mtbs_wildfires_CA_1993_2017
 arcpy.FeatureClassToFeatureClass_conversion("mtbs_lyr", arcpy.env.workspace, "mtbs_wildfires_CA_1993_2017", field_mapping=field_mappings)
@@ -69,7 +69,7 @@ arcpy.analysis.PairwiseIntersect("S_USA.Activity_StwrdshpCntrctng_PL;mtbs_wildfi
 # Set local variables
 input_features = [f for f in arcpy.ListFeatureClasses() if "_int" in f]
 field_mappings = arcpy.FieldMappings()
-keep_fields = ["ADMIN_FO_1", "ADMIN_DIST", "SUID", "FACTS_ID", "SUBUNIT", "ACTIVITY_N", "NBR_UNITS_", "NBR_UNITS1", "DATE_COMPL", "FY_COMPLET", "PRODUCTI_1", "LAND_SUI_1", "SUBUNIT_SI", "CRC_VALUE", "GIS_ACRES", "Incid_Name", "BurnBndAc", "Ig_Year", "Fire_Acres_R5"]
+keep_fields = ["ADMIN_FO_1", "ADMIN_DIST", "SUID", "FACTS_ID", "SUBUNIT", "ACTIVITY_N", "NBR_UNITS_", "NBR_UNITS1", "DATE_COMPL", "FY_COMPLET", "PRODUCTI_1", "LAND_SUI_1", "SUBUNIT_SI", "CRC_VALUE", "GIS_ACRES", "Incid_Name", "BurnBndAc", "Ig_Year", "Ig_Date", "Fire_Acres_R5"]
 output_feature = "Merged_Treatments"
 
 # Create field mappings
@@ -85,59 +85,8 @@ arcpy.AlterField_management(output_feature, "AREA_GEO", "Acres_within_Fire", "Ac
 
 ## Export subset of Activity_N related to reforestation
 # Select records where FY_COMPLET > Ig_Year and ACTIVITY_N meets the specified criteria
-where_clause = '"FY_COMPLET" > "Ig_Year" AND ("ACTIVITY_N" = \'Certification of Natural Regeneration without Site Prep\' Or "ACTIVITY_N" = \'Fill-in or Replant Trees\' Or "ACTIVITY_N" = \'Stocking Survey\' Or "ACTIVITY_N" = \'Plant Trees\' Or "ACTIVITY_N" = \'Salvage Cut (intermediate treatment, not regeneration)\' Or "ACTIVITY_N" = \'Site Preparation for Natural Regeneration - Burning\' Or "ACTIVITY_N" = \'Site Preparation for Natural Regeneration - Chemical\' Or "ACTIVITY_N" = \'Site Preparation for Natural Regeneration - Manual\' Or "ACTIVITY_N" = \'Site Preparation for Natural Regeneration - Mechanical\' Or "ACTIVITY_N" = \'Site Preparation for Planting - Mechanical\' Or "ACTIVITY_N" = \'Tree Release and Weed\')'
+where_clause = '"FY_COMPLET" > "Ig_Year" AND ("ACTIVITY_N" = \'Fill-in or Replant Trees\' Or "ACTIVITY_N" = \'Stocking Survey\' Or "ACTIVITY_N" = \'Plant Trees\' Or "ACTIVITY_N" = \'Salvage Cut (intermediate treatment, not regeneration)\' Or "ACTIVITY_N" = \'Site Preparation for Natural Regeneration - Burning\' Or "ACTIVITY_N" = \'Site Preparation for Natural Regeneration - Chemical\' Or "ACTIVITY_N" = \'Site Preparation for Natural Regeneration - Manual\' Or "ACTIVITY_N" = \'Site Preparation for Natural Regeneration - Mechanical\' Or "ACTIVITY_N" = \'Site Preparation for Planting - Mechanical\' Or "ACTIVITY_N" = \'Tree Release and Weed\')'
 arcpy.Select_analysis(output_feature, "Merged_Postfire", where_clause)
-
-
-## Summarize the burn severity in each reforestation subunit
-#
-for year in range(1993, 2018):
-    if year == 1998:    # no postfire reforestation for fires in 1998
-        continue
-    # Select records from Merged_Postfire where Ig_Year is equal to the current year
-    where_clause = "Ig_Year = {}".format(year)
-    arcpy.SelectLayerByAttribute_management("Merged_Postfire", "NEW_SELECTION", where_clause)
-
-    # Use Zonal Statistics as Table tool with Merged_Postfire as feature zone data
-    input_raster = "mtbs_CA_{}.tif".format(year)
-    output_table = "zonalst_merged_{}".format(year)
-    arcpy.sa.ZonalStatisticsAsTable("Merged_Postfire", "SUID", input_raster, output_table, "NODATA", "ALL")
-    arcpy.management.CalculateField(output_table, "Ig_Year", "{}".format(year), "PYTHON3", '', "LONG", "NO_ENFORCE_DOMAINS")
-
-# Set the local variables
-outTable = "zonalst_merged_allyears"
-fieldMappings = arcpy.FieldMappings()
-keepFields = ["OBJECTID", "SUID", "Ig_Year", "MIN", "MAX", "MEAN", "MEDIAN", "MAJORITY", "MINORITY"]
-
-# Find all tables containing the text "zonalst_merged" in the name
-tableList = [table for table in arcpy.ListTables() if "zonalst_merged" in table]
-
-# Add the tables to the field mappings and retain only the specified fields
-for table in tableList:
-    fieldMappings.addTable(table)
-    for field in fieldMappings.fields:
-        if field.name not in keepFields:
-            fieldMappings.removeFieldMap(fieldMappings.findFieldMapIndex(field.name))
-
-# Merge the tables
-arcpy.Merge_management(tableList, outTable, fieldMappings)
-
-# # Round MEAN field to nearest single digit
-# with arcpy.da.UpdateCursor("zonalst_merged_allyears", ["MEAN"]) as cursor:
-#     for row in cursor:
-#         row[0] = round(row[0])
-#         cursor.updateRow(row)
-
-## Join zonal stats table with Merged_Postfire
-# Set the local variables
-in_data = "Merged_Postfire"
-in_field = "SUID"
-join_table = "zonalst_merged_allyears"
-join_field = "SUID"
-
-# Join the two tables by the SUID field
-arcpy.JoinField_management(in_data, in_field, join_table, join_field)
-
 
 
 ## Count Overlapping Features for all records in all postfire activities
@@ -170,6 +119,91 @@ arcpy.TableToTable_conversion(output_table, env.workspace, "Overlap_Table_Joined
 
 # Join "OVERLAP_OID" in Overlap_Table_Joined with "OBJECTID" in Overlap_feature
 arcpy.management.AddJoin("Overlap_Table_Joined", "OVERLAP_OID", output_feature, "OBJECTID", "KEEP_ALL", "NO_INDEX_JOIN_FIELDS")
+
+# Join "OBJECTID" in Overlap_Feature_Joined with "OVERLAP_OID" in Overlap_Table
+arcpy.management.AddJoin(output_feature, "OBJECTID", output_table, "OVERLAP_OID", "KEEP_ALL", "NO_INDEX_JOIN_FIELDS")
+arcpy.management.CopyFeatures("Overlap_Feature", "Overlap_Feature_Joined", '', None, None, None)
+
+
+## Summarize the burn severity in each reforestation subunit using TABULATE AREA and ZONAL STATS AS TABLE
+#
+for year in range(1994, 2018):
+    if year == 1998:    # no postfire reforestation for fires in 1998
+        continue
+    # Select records from Overlap_Feature where Ig_Year is equal to the current year
+    where_clause = "Ig_Year = {}".format(year)
+    where_clause_ff = "Merged_Postfire_Ig_Year = {}".format(year)
+    arcpy.SelectLayerByAttribute_management("Overlap_Feature_Joined", "NEW_SELECTION", where_clause_ff)
+
+    # Use Tabulate Area tool with OVERLAP_OID as feature zone data
+    input_zone = "Overlap_Feature_Joined"
+    zone_field = "Overlap_Table_OVERLAP_OID"
+    input_raster = "mtbs_CA_{}.tif".format(year)
+    class_field = "Value"
+    output_tabulate = "tabulate_merged_{}".format(year)
+    output_zonal = "zonal_merged_{}".format(year)
+    
+    # ZONAL STATS
+    arcpy.sa.ZonalStatisticsAsTable(input_zone, zone_field, input_raster, output_zonal, "NODATA", "ALL")
+    
+    # Add field to output_zonal that equals value of Overlap_Table_OVERLAP_OID field for that record
+    arcpy.management.AddField(output_zonal, "Overlap_Table_OVERLAP_OID", "LONG")
+    arcpy.management.CalculateField(output_zonal, "Overlap_Table_OVERLAP_OID", "!{}!".format(zone_field), "PYTHON3")
+    
+    # Add Ig_Year field to output_zonal and set its value to the current year   
+    arcpy.management.CalculateField(output_zonal, "Ig_Year", "{}".format(year), "PYTHON3", '', "LONG", "NO_ENFORCE_DOMAINS")
+    
+    # TABULATE AREA
+    arcpy.sa.TabulateArea(input_zone, zone_field, input_raster, class_field, output_tabulate)
+    arcpy.management.CalculateField(output_tabulate, "Ig_Year", "{}".format(year), "PYTHON3", '', "LONG", "NO_ENFORCE_DOMAINS")
+
+
+
+# Find all tables containing the text "tabulate_merged" in the name and merge them
+outTable = "tabulate_allyears"
+tableList = [table for table in arcpy.ListTables() if "tabulate_merged" in table]
+# Merge the annual tables
+arcpy.Merge_management(tableList, outTable)
+
+
+# Find all tables containing the text "zonal_merged" in the name and merge them
+
+
+# Set the local variables
+outTable = "zonal_allyears"
+fieldMappings = arcpy.FieldMappings()
+keepFields = ["OBJECTID", "Overlap_Table_OVERLAP_OID", "Ig_Year", "MEAN", "MEDIAN", "MAJORITY"]
+
+# Find all tables containing the text "zonalst_merged" in the name
+tableList = [table for table in arcpy.ListTables() if "zonal_merged" in table]
+
+# Add the tables to the field mappings and retain only the specified fields
+for table in tableList:
+    fieldMappings.addTable(table)
+    for field in fieldMappings.fields:
+        if field.name not in keepFields:
+            fieldMappings.removeFieldMap(fieldMappings.findFieldMapIndex(field.name))
+
+# Merge the annual zonal tables
+arcpy.Merge_management(tableList, outTable, fieldMappings)
+
+
+# Join the merged Tabulate & Zonal tables by the OVERLAP_OID fields
+in_data = "Overlap_Feature_Joined"
+in_field_zonal = "Overlap_Table_OVERLAP_OID"
+in_field_tabulate = "Overlap_Table_OVERLAP_OID"
+
+join_table_zonal = "zonal_allyears"
+join_field_zonal = "Overlap_Table_OVERLAP_OID"
+join_table_tabulate = "tabulate_allyears"
+join_field_tabulate = "OVERLAP_TABLE_OVERLAP_OID"
+
+arcpy.management.MakeFeatureLayer(in_data, "input_lyr")
+
+arcpy.management.AddJoin(in_data, in_field_tabulate, join_table_tabulate, join_field_tabulate, "KEEP_ALL")
+arcpy.management.AddJoin(in_data, in_field_zonal, join_table_zonal, join_field_zonal, "KEEP_ALL")
+arcpy.management.CopyRows("input_lyr", Overlap_Feature_Joined_Severity)
+
 
 # Export table to csv to be used for overlap summarization
 arcpy.TableToTable_conversion("Overlap_Table_Joined", r"C:\Users\smithke3\OneDrive - Oregon State University\Kelly\Data", "Postfire_Overlaps.csv")
